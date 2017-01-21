5e3a43dd13282c52d82a96cdb40347cf83688121
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2017-01-21 13:57:22 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
@@ -33,6 +33,7 @@ public class InputActivity extends AppCompatActivity {
         etLokasi = (EditText) findViewById(R.id.editTextLokasi);
         ivFoto = (ImageView) findViewById(R.id.imageViewFoto);
         ivFoto.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 pickPhoto();
@@ -45,6 +46,22 @@ public class InputActivity extends AppCompatActivity {
                         doSave();
                     }
                 });
+        hotel = (Hotel) getIntent().getSerializableExtra(MainActivity.HOTEL);
+        if (hotel != null) {
+            setTitle("Edit " + hotel.judul);
+            fillData();
+        } else {
+            setTitle("New Hotel");
+        }
+    }
+
+    private void fillData() {
+        etJudul.setText((hotel.judul));
+        etDeskripsi.setText(hotel.deskripsi);
+        etDetail.setText(hotel.detail);
+        etLokasi.setText(hotel.lokasi);
+        uriFoto = Uri.parse(hotel.foto);
+        ivFoto.setImageURI(uriFoto);
     }
 
     private void doSave() {
